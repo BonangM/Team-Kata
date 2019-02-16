bdb99476a9ec4d2970b134fef38f3c536ca0a295
BonangM <201318969@student.uj.ac.za>
2019-02-16 19:56:11 +0200

    GCD class
    Functions to calculate gcd

---
+++ KataFunctions/GCD.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace KataFunctions
+{
+  public class Gcd
+  {
+    public int[] numbers;
+
+    public int calculateGcd(int[] givenNumbers)
+    {
+      //use Linq's Aggregate function to accumulate the gcd forumla to gcd(a,b,c)= gcd(a,(gcd(b,c))
+      return givenNumbers.Aggregate(gcdFormula);
+    }
+
+    static int gcdFormula(int a, int b)
+    {
+      if (b == 0)
+      {
+        return a;
+      }
+      //Euclidean algorithm
+      return gcdFormula(b, a % b);
+    }
+  }
+}
